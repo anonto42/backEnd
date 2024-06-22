4f86716ca89022ed499e40d2eadb946138276b1a
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-06-23 01:08:42 +0600

    script updated

--- src/script.js
+++ src/script.js
@@ -1,6 +1,11 @@
 import express from 'express';
 const app = express();
 
+// app.use(express.static(''));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.use((req, res, next)=>{
     console.log("everything was good!");
     next();
