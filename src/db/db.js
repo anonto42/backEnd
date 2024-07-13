95b3fd90cfb7d2d96caf550fe1d01f990bb2c198
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-07-13 16:20:25 +0600

    DB Configed

--- src/db/db.js
+++ src/db/db.js
@@ -1,1 +1,31 @@
-import mongoose from "mongoose";
+import mongoose, { connections } from "mongoose";
+import { DB_NAME } from './../utils/const.js';
+
+
+
+const connectDB = async () => {
+    try {
+        const DB = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+        console.log(`MongoDB connected successfully to ${DB_NAME}`);
+        console.log(`your MONGODB connected location in ${DB.connection.host} `);
+
+        // console.log(' all about DB');
+        // console.log(DB);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+export default connectDB;
+
+
+// ;(async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     } catch (error) {
+//         console.log(error);
+//         throw error
+//     }
+// })();
