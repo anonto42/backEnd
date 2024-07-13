f814cf8c001851360527e7ce538816e13ed4f0bf
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-07-13 16:20:40 +0600

    new code added

--- src/script.js
+++ src/script.js
@@ -1,15 +1,46 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './db/db.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+// Express initialization
 const app = express();
 
-// app.use(express.static(''));
+
+// configuration's
 
 dotenv.config();
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    Credential: true
+}));
+app.use(cookieParser());
+app.use(express.json({limit:"10kb"}));
+app.use(express.urlencoded({limit:"10kb",extended:true}));
+app.use(express.static("public"));
+
+
+// DB connection
+connectDB()
+.then(
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log('');
+        console.log(`Server is running on port ${process.env.PORT}`);
+        console.log('');
+    })
+)
+.catch((err)=>{
+    console.log(`ERROR : ${err}`);
+})
 
 
 
-app.listen(process.env.PORT,function(){
-    console.log(`surver is runing on ${process.env.PORT}`);
-});
+
+
+
+// app.listen(process.env.PORT,function(){
+//     console.log('');
+//     console.log(`surver is runing on ${process.env.PORT}`);
+//     console.log('');
+// });
